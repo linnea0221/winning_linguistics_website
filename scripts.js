18ad2882623192b0f1027f76fa76f6782654eb6f
eleanorcook <57177098+eleanorcook@users.noreply.github.com>
2019-12-05 19:43:06 +0000

    Fixed the buttons on fun_facts
    In addition played around with the coloums and know what we're doing for the carousel

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Click me: " + count;
-};
+var count;
+var count_2;
+
+console.log('helllllo')
+ var button = document.getElementById("clickme"),
+   count = 0;
+ button.onclick = function() {
+   console.log('clicked')
+   count += 1;
+   button.innerHTML = "How many people like this fact: " + count;
+ };
+
+ var button_2 = document.getElementById("clickme2"),
+   count_2 = 0;
+ button_2.onclick = function() {
+   count_2 += 1;
+   button_2.innerHTML = "How many people like this: " + count_2;
+ };
