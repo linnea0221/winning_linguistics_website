28714c3a40ac9313b9347a9ca080ee41cdee4bf0
linnea0221 <57177110+linnea0221@users.noreply.github.com>
2019-12-12 16:08:20 +0000

    fixed the button names

--- scripts.js
+++ scripts.js
@@ -7,21 +7,21 @@ console.log('helllllo')
  button.onclick = function() {
    console.log('clicked')
    count += 1;
-   button.innerHTML = "How many people like this fact: " + count;
+   button.innerHTML = "Number of times you've liked this fact: " + count;
  };
 
  var button_2 = document.getElementById("clickme2"),
    count_2 = 0;
  button_2.onclick = function() {
    count_2 += 1;
-   button_2.innerHTML = "How many people like this fact: " + count_2;
+   button_2.innerHTML = "Number of times you've liked this fact: " + count_2;
  };
 
  var button_3 = document.getElementById("clickme3"),
    count_3 = 0;
  button_3.onclick = function() {
    count_3 += 1;
-   button_3.innerHTML = "How many people like this fact: " + count_3;
+   button_3.innerHTML = "Number of times you've liked this fact: " + count_3;
  };
 
 
